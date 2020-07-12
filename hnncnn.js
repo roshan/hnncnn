e4c29ab3c0ab056208923d24571cceb4197d1444
Roshan George <roshan@arjie.com>
2020-07-12 12:58:09 -0700

    Guard against posts without all options
    Advertisement posts don't have comments

--- hnncnn.js
+++ hnncnn.js
@@ -1,1 +1,8 @@
-Array.from(document.getElementsByClassName("subtext")).forEach(x => {x.children[1].style.display = "none"; x.children[6].style.display = "none";})
+Array.from(document.getElementsByClassName("subtext")).forEach(x => {
+  if (x.children.length > 1) {
+    x.children[1].style.display = "none";
+  }
+  if (x.children.length > 6) {
+    x.children[6].style.display = "none";
+  }
+})
